ab3aba4962bb48f515252a1dc4a613afedcf8cff
OrTem76 <tensorrab6@gmail.com>
2026-02-17 23:05:46 +0300

    Initial commit

--- src/test/java/DemoqaHomeWork1.java
+++ src/test/java/DemoqaHomeWork1.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.open;
 
 public class DemoqaHomeWork1 {
 
